b3418674998fe2823a56b9b02b690c1827b6f772
Michael <michael.feldman09@gmail.com>
2020-06-30 14:33:09 -0400

    Added plane cut shader and interactivity for interior design. Added splash animator and interactivity for splash monitor

---
+++ PortfolioUnity/Assets/_Scripts/HouseRotator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseRotator : MonoBehaviour
+{
+    public Transform _house;
+    bool _rotating = false;
+    public float _rotTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RotateHouse()
+    {
+        if(!_rotating)
+            StartCoroutine(RotateHouseR());
+    }
+
+    IEnumerator RotateHouseR()
+    {
+        _rotating = true;
+        Quaternion cur = _house.rotation;
+        _house.Rotate(Vector3.up * 90f);
+        Quaternion fin = _house.rotation;
+        _house.rotation = cur;
+        float timer = 0;
+        while(timer < _rotTime)
+        {
+            _house.rotation = Quaternion.Slerp(cur, fin, timer / _rotTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        _house.rotation = fin;
+        _rotating = false;
+    }
+}
---
+++ PortfolioUnity/Assets/_Scripts/SplashAnimator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashAnimator : MonoBehaviour
+{
+    public Text _logoText;
+    public RawImage _logoImg;
+    public CanvasGroup _fader;
+    bool _animating;
+    public float _fadeDur;
+    public float _holdDur;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnimateLogo()
+    {
+        if(!_animating)
+            StartCoroutine(AnimateLogoR());
+    }
+
+    IEnumerator AnimateLogoR()
+    {
+        _animating = true;
+        Color newColor = Color.HSVToRGB(Random.value, 1, 1);
+        _logoText.color = newColor;
+        _logoImg.color = newColor;
+        float timer = 0;
+        while(timer < _fadeDur)
+        {
+            _fader.alpha = 1 - timer / _fadeDur;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        _fader.alpha = 0;
+        yield return new WaitForSeconds(_holdDur);
+        timer = 0;
+        while (timer < _fadeDur)
+        {
+            _fader.alpha = timer / _fadeDur;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        _fader.alpha = 1;
+        _animating = false;
+    }
+}
